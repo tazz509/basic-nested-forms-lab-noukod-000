5156452858cb266ffbc111d4bc66f862e6669e9f
carly <carly@noukod.com>
2019-01-30 15:46:04 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'Salt')
+    @recipe.ingredients.build(name: 'Hot Pepper')
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    # puts @recipe.inspect
+    # puts recipes_params.inspect
+    redirect_to @recipe
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+    ingredients_attributes: [
+      :name, :quantity
+      ])
+ end
 end
